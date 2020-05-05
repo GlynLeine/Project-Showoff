db0c9bda3593d77639cd93817bbae00ec57ffda2
GlynLeine <g.m.leine@gmail.com>
2020-05-05 10:08:23 +0200

    Created planet earth view prototype
    [Add] planet behaviour
    [Add] SimpleOrbit behaviour
    [Add] Orbit behaviour (physics based)
    [Add] Sun material
    [Add] Starry skybox
    [Add] Moon material
    [Add] Earth material

---
+++ Strategic space test/Assets/Orbit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Orbit : MonoBehaviour
+{
+    static List<Orbit> orbits;
+    static float G;
+
+    public Vector3 initialVelocity;
+    public float gravitationalConstant = 0.000000000667f;
+    public new Rigidbody rigidbody;
+
+    private void OnValidate()
+    {
+        if (gravitationalConstant != G)
+            G = gravitationalConstant;
+    }
+
+    static Orbit()
+    {
+        orbits = new List<Orbit>();
+    }
+
+    private void Start()
+    {
+        orbits.Add(this);
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = initialVelocity;
+    }
+
+    private void Update()
+    {
+        foreach (Orbit orbit in orbits)
+            if (orbit != this)
+            {
+                Vector3 forceNormal = (orbit.transform.position - transform.position);
+                Vector3 F = (((G * rigidbody.mass) / forceNormal.sqrMagnitude) * orbit.rigidbody.mass) * forceNormal.normalized;
+                rigidbody.velocity += F;
+            }
+    }
+}
---
+++ Strategic space test/Assets/SimpleOrbit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleOrbit : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float range;
+    public Vector3 right;
+
+    void Start()
+    {
+        range = (target.position - transform.position).magnitude;
+        right = transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * range / 10000 * transform.up;
+        Vector3 normal = (transform.position - target.position).normalized;
+        transform.position = target.position + (normal * range);
+        transform.rotation = Quaternion.LookRotation(-normal, Vector3.Cross(right, normal));
+    }
+}
