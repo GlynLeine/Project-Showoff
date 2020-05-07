5fbf00196c0bf3416fed45f4bf3c1d2932972653
GlynLeine <g.m.leine@gmail.com>
2020-05-07 14:28:00 +0200

    building placement prototype
    [Add] buildingl locations
    [Add] Building placer
    [Add] Building system
    [Add] gamemanager

---
+++ ProjectShowoff/Assets/Scripts/BuildingPlacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+    public LocationType locationType;
+
+    public GameObject buildingPrefab;
+
+    private BuildingSystem system;
+
+    private void Start()
+    {
+        system = FindObjectOfType<BuildingSystem>();
+    }
+
+    public void Place()
+    {
+        system.PlaceBuilding(this);
+    }
+}
---
+++ ProjectShowoff/Assets/Scripts/BuildingSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LocationType
+{
+    Rural, Coastal
+}
+
+public class BuildingSystem : MonoBehaviour
+{
+    Dictionary<LocationType, List<BuildingLocation>> locations = new Dictionary<LocationType, List<BuildingLocation>>();
+
+    public Transform planet;
+
+    public void ReportLocation(BuildingLocation location)
+    {
+        foreach (var locType in locations)
+        {
+            if (locType.Value.Contains(location))
+                locType.Value.Remove(location);
+        }
+
+        if (!locations.ContainsKey(location.locationType))
+            locations.Add(location.locationType, new List<BuildingLocation>());
+
+        location.transform.parent = planet;
+        location.transform.up = (location.transform.position - planet.position).normalized;
+        locations[location.locationType].Add(location);
+    }
+
+    public void PlaceBuilding(BuildingPlacer buildingData)
+    {
+        List<BuildingLocation> locationsOfType = locations[buildingData.locationType];
+        if (locationsOfType.Count > 0)
+        {
+            BuildingLocation location = locationsOfType[Random.Range(0, locationsOfType.Count)];
+            Instantiate(buildingData.buildingPrefab, location.transform);
+            locationsOfType.Remove(location);
+            Debug.Log("placed a building on a " + buildingData.locationType.ToString() + " location.");
+        }
+        else
+        {
+            Debug.Log("no space");
+        }
+    }
+}
