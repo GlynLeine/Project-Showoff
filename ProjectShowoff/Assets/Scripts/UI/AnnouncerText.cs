748f75fb098cc2dd571b3fa25e4dc7777c75d3a2
Rick Oosthof <roosthof@gmail.com>
2020-05-20 16:58:58 +0200

    added zoom & announcer scripts
    zoom fade script only & the initial announcer script

---
+++ ProjectShowoff/Assets/Scripts/UI/AnnouncerText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnouncerText : MonoBehaviour
+{
+    public int announcerTextSpeed = 1;
+    [SerializeField] private GameObject announcerText = null;
+    private Vector3 announcerPosition;
+    private RectTransform announcerRectTransform;
+    private float announcerTextWidth;
+
+    private void Start()
+    {
+        announcerRectTransform = announcerText.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        announcerText.transform.Translate(-announcerTextSpeed*Time.deltaTime,0,0);
+        announcerPosition = announcerText.transform.position;
+        announcerTextWidth = announcerRectTransform.sizeDelta.x;
+        if (announcerPosition.x < -announcerTextWidth/2 + 200)
+        {
+            announcerPosition.x = (announcerTextWidth/2) - 60;
+        }
+        announcerText.transform.position = announcerPosition;
+    }
+}
---
+++ ProjectShowoff/Assets/Scripts/UI/ZoomBarScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomBarScript : MonoBehaviour
+{
+    [SerializeField] private Image sliderBg = null;
+    [SerializeField] private Image sliderFill = null;
+
+    private float timer;
+
+    public void SliderValueChange()
+    {
+        timer = 0;
+        InvokeRepeating("TimerRepeat", 2.5f, 0.1f);
+    }
+
+    private void TimerRepeat()
+    {
+        timer += 0.1f;
+        Color bgC = sliderBg.color;
+        Color fillC = sliderFill.color;
+        bgC.a = 1 - 0.025f * timer;
+        fillC.a = 1 - 0.025f * timer;
+        sliderBg.color = bgC;
+        sliderFill.color = fillC;
+        if (timer > 40)
+        {
+            CancelInvoke("TimerRepeat");
+        }
+    }
+
+    public void SliderAlphaChange()
+    {
+        Color bgC = sliderBg.color;
+        Color fillC = sliderFill.color;
+        bgC.a = 1;
+        fillC.a = 1;
+        sliderBg.color = bgC;
+        sliderFill.color = fillC;
+    }
+}
