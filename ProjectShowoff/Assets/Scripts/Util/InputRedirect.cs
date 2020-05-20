9272a55e0e7634379d19dabfe5c61dc70f867cdd
GlynLeine <g.m.leine@gmail.com>
2020-05-20 11:02:53 +0200

    Add InputRedirect.inputOverUI

--- ProjectShowoff/Assets/Scripts/Util/InputRedirect.cs
+++ ProjectShowoff/Assets/Scripts/Util/InputRedirect.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InputRedirect : MonoBehaviour
 {
+    public EventSystem eventSystem;
+    public GraphicRaycaster raycaster;
+
     static private Vector2 previousInputPos;
     static private float previousTouchZoom;
     static private bool touchZoomReset;
@@ -13,8 +18,23 @@ public class InputRedirect : MonoBehaviour
     static public bool pressed;
     static public float zoom;
 
+    static public bool inputOverUI;
+
     private void Update()
     {
+        //Set up the new Pointer Event
+        PointerEventData pointerEventData = new PointerEventData(eventSystem);
+        //Set the Pointer Event Position to that of the mouse position
+        pointerEventData.position = Input.mousePosition;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        raycaster.Raycast(pointerEventData, results);
+
+        inputOverUI = results.Count > 0;
+
         if (Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
