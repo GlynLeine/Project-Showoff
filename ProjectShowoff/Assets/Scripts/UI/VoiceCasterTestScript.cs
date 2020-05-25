228a60c60442e508febb2ee3cef5305346b164e3
Rick Oosthof <roosthof@gmail.com>
2020-05-25 16:20:50 +0200

    added variable text movement
    announcer text moves better now and reacts no matter how small or big it is

---
+++ ProjectShowoff/Assets/Scripts/UI/VoiceCasterTestScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceCasterTestScript : MonoBehaviour
+{
+    [SerializeField] private GameObject textScript = null;
+    private AnnouncerText atScript;
+    private float timer = 0;
+    void Start()
+    {
+        atScript = textScript.GetComponent<AnnouncerText>();
+    }
+
+    private void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        if (timer > 5)
+        {
+            atScript.TextChanger("changing it to a really long paragraph real quick that has like looooooads of words but look it still comes back right on time!");
+            timer = 0;
+        }
+    }
+}
--- ProjectShowoff/Assets/Scripts/UI/ZoomBarScript.cs
+++ ProjectShowoff/Assets/Scripts/UI/ZoomBarScript.cs
@@ -8,38 +8,26 @@ public class ZoomBarScript : MonoBehaviour
 {
     [SerializeField] private Image sliderBg = null;
     [SerializeField] private Image sliderFill = null;
-
     private float timer;
-
     public void SliderValueChange()
     {
         timer = 0;
-        CancelInvoke("TimerRepeat");
-        InvokeRepeating("TimerRepeat", 2.5f, 0.1f);
+        CancelInvoke();
+        InvokeRepeating("SliderAlphaChange",2.5f,0.05f);
     }
 
-    private void TimerRepeat()
+    private void SliderAlphaChange()
     {
         timer += 0.1f;
         Color bgC = sliderBg.color;
         Color fillC = sliderFill.color;
-        bgC.a = 1 - 1f * timer;
-        fillC.a = 1 - 1f * timer;
+        bgC.a = 1-timer;
+        fillC.a = 1-timer;
         sliderBg.color = bgC;
         sliderFill.color = fillC;
         if (timer > 1)
         {
-            CancelInvoke("TimerRepeat");
+            CancelInvoke();
         }
-    }
-
-    public void SliderAlphaChange()
-    {
-        Color bgC = sliderBg.color;
-        Color fillC = sliderFill.color;
-        bgC.a = 1;
-        fillC.a = 1;
-        sliderBg.color = bgC;
-        sliderFill.color = fillC;
     }
 }
