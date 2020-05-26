bd3aac5dfb880b0efea80ad888c157d46a5ae8f4
GlynLeine <g.m.leine@gmail.com>
2020-05-26 10:14:35 +0200

    clouds improvement
    [Change] clouds fade on distance from camera direction ray
    [Change] clouds now use 2 different cloud models
    [Change] cloud fade dependent on object position instead of fragment position

--- ProjectShowoff/Assets/Scripts/Effects/CloudEffect.cs
+++ ProjectShowoff/Assets/Scripts/Effects/CloudEffect.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class CloudEffect : MonoBehaviour
 {
     public GameObject cloudPrefab;
+    public GameObject altCloudPrefab;
 
-    public int numberOfClouds = 100;
+    public int resolution = 100;
     public float cloudHeight = 1.5f;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
         float angleIncrement = Mathf.PI * 2 * goldenRatio;
 
-        for (int i = 0; i < numberOfClouds; i++)
+        for (int i = 0; i < resolution; i++)
         {
-            float step = (float)i / numberOfClouds;
+            float step = (float)i / resolution;
             float phi = Mathf.Acos(1 - 2 * step);
             float theta = angleIncrement * i;
 
@@ -28,7 +28,9 @@ public class CloudEffect : MonoBehaviour
 
             offset *= cloudHeight;
 
-            GameObject cloud = Instantiate(cloudPrefab, transform.position + offset, Quaternion.identity, transform);
+            bool alt = Random.Range(0f, 1f) > 0.5f;
+            GameObject cloud = Instantiate(alt ? altCloudPrefab : cloudPrefab, transform.position + offset, Quaternion.identity, transform);
+            cloud.transform.up = offset.normalized;
             //cloud.isStatic = true;
         }
     }
