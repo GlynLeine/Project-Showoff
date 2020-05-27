b3c40ebe0e9bdf91da4f8aee70154191eb6b937f
Rick Oosthof <roosthof@gmail.com>
2020-05-27 09:53:16 +0200

    added timer code base
    see title

--- ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
+++ ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
@@ -11,19 +11,23 @@ public class YearTimer : MonoBehaviour
         timer += 1 * Time.deltaTime;
         if (timer <= 60)
         {
-            
+            //spring 1
         }
-        if (timer > 60)
+        else if (timer > 60 && timer <= 120)
         {
-            if (timer <= 120)
-            {
-                
-            }
-
-            if (timer > 120)
-            {
-                
-            }
+            //summer
+        }
+        else if (timer > 120 && timer <= 180)
+        {
+            //fall
+        }
+        else if (timer > 180 && timer <= 240)
+        {
+            //winter
+        }
+        else if (timer > 240 && timer <= 300)
+        {
+            //spring 2
         }
     }
 }
