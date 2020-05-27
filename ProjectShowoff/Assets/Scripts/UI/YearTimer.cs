97dde0c8f8ee79e06f0866be791198e068e62a59
Rick Oosthof <roosthof@gmail.com>
2020-05-27 09:49:34 +0200

    year icons
    5 seasons in a year

--- ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
+++ ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class YearTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timer = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += 1 * Time.deltaTime;
+        if (timer <= 60)
+        {
+            
+        }
+        if (timer > 60)
+        {
+            if (timer <= 120)
+            {
+                
+            }
+
+            if (timer > 120)
+            {
+                
+            }
+        }
     }
 }
