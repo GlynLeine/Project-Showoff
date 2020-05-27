54abcbf20fa638e22eab479b7b5904e9e52d5bdb
Rick Oosthof <roosthof@gmail.com>
2020-05-27 10:12:15 +0200

    80x80 spring test
    tested to see if this looks good

--- ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
+++ ProjectShowoff/Assets/Scripts/UI/YearTimer.cs
@@ -1,33 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class YearTimer : MonoBehaviour
 {
     private float timer = 0;
+    [SerializeField] private Image spring1GrayscaleImage = null;
+    [SerializeField] private Image summerGrayscaleImage = null;
+    [SerializeField] private Image fallGrayscaleImage = null;
+    [SerializeField] private Image winterGrayscaleImage = null;
+    [SerializeField] private Image spring2GrayscaleImage = null;
 
     void Update()
     {
         timer += 1 * Time.deltaTime;
         if (timer <= 60)
         {
-            //spring 1
+            spring1GrayscaleImage.fillAmount = 1 - timer / 60;
         }
         else if (timer > 60 && timer <= 120)
         {
-            //summer
+            summerGrayscaleImage.fillAmount = 1 - (timer-60) / 60;
         }
         else if (timer > 120 && timer <= 180)
         {
-            //fall
+            fallGrayscaleImage.fillAmount = 1 - (timer-120) / 60;
         }
         else if (timer > 180 && timer <= 240)
         {
-            //winter
+            winterGrayscaleImage.fillAmount = 1 - (timer-180) / 60;
         }
         else if (timer > 240 && timer <= 300)
         {
-            //spring 2
+            spring2GrayscaleImage.fillAmount = 1 - (timer-240) / 60;
         }
     }
 }
