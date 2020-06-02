d0c84ccbf72766b986e72c70f4e2db74fea0b087
Robert O'Connor <445921@student.saxion.nl>
2020-06-02 14:36:32 +0200

    Most of poking complete

---
+++ ProjectShowoff/Assets/Scripts/Characters/Pokable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokable : MonoBehaviour
+{
+    public bool pokePlay = false;
+
+    public void poke()
+    {
+        //fmod code needed
+        pokePlay = true; 
+    }
+}
--- ProjectShowoff/Assets/Scripts/Characters/Poke.cs
+++ ProjectShowoff/Assets/Scripts/Characters/Poke.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Poke : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class Poke : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (InputRedirect.pressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(InputRedirect.inputPos);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Pokable pokable = hit.collider.gameObject.GetComponent<Pokable>();
+                if (pokable != null)
+                {
+                    pokable.poke();
+                    Debug.Log("Poked");
+                }
+            }
+        }
     }
+
+ 
 }
