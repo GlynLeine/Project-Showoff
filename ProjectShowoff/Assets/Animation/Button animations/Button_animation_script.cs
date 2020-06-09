b6831e60423f6e30758bfdf628a820806e8ac420
Tanya Schrijver <tanya.schrijver@gmail.com>
2020-06-09 15:10:04 +0200

    [Change] Button animation work

---
+++ ProjectShowoff/Assets/Animation/Button animations/Button_animation_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_animation_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    public void button_touch()
+    {
+        GetComponent<Animator>().Play("");
+    }
+}
