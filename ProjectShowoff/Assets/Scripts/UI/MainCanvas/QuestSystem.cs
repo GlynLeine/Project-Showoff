e87d27b0ff882ef121365096980cc817a2388ec4
Rick Oosthof <roosthof@gmail.com>
2020-06-15 14:53:52 +0200

    innitial non working quest system

---
+++ ProjectShowoff/Assets/Scripts/UI/MainCanvas/QuestSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSystem : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(BuildingSystem.onBuildingPlaced);
+    }
+    IEnumerator QuestQueueSystem()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+        }
+    }
+    private void OnBuildingPlaced(BuildingLocation location, BuildingPlacer buildingData, Building building)
+    {
+    }
+
+    IEnumerator TemporaryUpdate()
+    {
+        yield return null;
+    }
+}
