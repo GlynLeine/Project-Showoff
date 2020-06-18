3975caa63b65c7cfdb6a6fd8926aa91fceb45380
GlynLeine <g.m.leine@gmail.com>
2020-06-18 16:58:11 +0200

    trainstation level up

---
+++ ProjectShowoff/Assets/Scripts/Effects/TrainStation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainStation : MonoBehaviour
+{
+    public GameObject trainStation;
+    public GameObject airPort;
+    public GameObject spaceCenter;
+
+    private int lvl;
+    public int level
+    {
+        get => lvl;
+        set
+        {
+            lvl = value;
+
+            if (lvl <= 0)
+            {
+                trainStation.SetActive(true);
+                airPort.SetActive(false);
+                spaceCenter.SetActive(false);
+            }
+            else if (lvl == 1)
+            {
+                trainStation.SetActive(false);
+                airPort.SetActive(true);
+                spaceCenter.SetActive(false);
+            }
+            else if (lvl > 1)
+            {
+                trainStation.SetActive(false);
+                airPort.SetActive(true);
+                spaceCenter.SetActive(false);
+            }
+        }
+    }
+}
