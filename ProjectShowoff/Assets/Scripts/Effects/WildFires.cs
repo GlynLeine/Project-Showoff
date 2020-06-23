8eaaf402f87c4a7bab317a1baede5cdcc86b5542
GlynLeine <g.m.leine@gmail.com>
2020-06-23 10:10:48 +0200

    wildfires turn off

--- ProjectShowoff/Assets/Scripts/Effects/WildFires.cs
+++ ProjectShowoff/Assets/Scripts/Effects/WildFires.cs
@@ -1,38 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WildFires : MonoBehaviour
 {
-    List <WildFire_Get> wildfires;
-
+    List<WildFire_Get> wildfires;
+    bool fire;
     private void Start()
     {
-        wildfires = new List <WildFire_Get> (FindObjectsOfType<WildFire_Get>());
+        wildfires = new List<WildFire_Get>(FindObjectsOfType<WildFire_Get>());
 
         foreach (WildFire_Get wildfire in wildfires)
             wildfire.gameObject.SetActive(false);
 
-        StartCoroutine(Spawn());
+        fire = false;
     }
 
-    IEnumerator Spawn()
+    private void Update()
     {
-        while (GameManager.ozone < 0.5)
-            yield return null;
+        if (!fire && GameManager.ozone > 0.7f)
+            StartCoroutine(Enable(true));
+        else if (fire && GameManager.ozone <= 0.7f)
+            StartCoroutine(Enable(false));
+    }
 
-        while (wildfires.Count > 0)
+    IEnumerator Enable(bool enable)
+    {
+        fire = enable;
+        int burnCount = 0;
+        while (burnCount < wildfires.Count)
         {
             for (int i = 0; i < wildfires.Count; i++)
             {
-                if (Random.value < 0.1f * GameManager.deltaTime)
-                {                    
-                    wildfires[i].Enable();
-                    wildfires.RemoveAt(i);
-                    i--;
-                }
+                if (wildfires[i].burning != enable)
+                    if (Random.value < 0.01f * GameManager.deltaTime)
+                    {
+                        if (enable)
+                            wildfires[i].Enable();
+                        else
+                            wildfires[i].Disable();
+
+                        burnCount++;
+                    }
             }
-            yield return null; 
+            yield return null;
         }
     }
 }
