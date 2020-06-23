ed972ad253aecb1219eb4f1552e9c193834b5863
Rick Oosthof <roosthof@gmail.com>
2020-06-23 21:02:18 +0200

    added icon changer for train station + fix for it on tutorial, updated main canvas

---
+++ ProjectShowoff/Assets/Scripts/UI/IconChangerOnEnable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconChangerOnEnable : MonoBehaviour
+{
+    public Sprite airPortSprite;
+    public Sprite spaceStationSprite;
+    public Sprite trainStationSprite;
+    private Image thisImage;
+    void Start()
+    {
+        thisImage = gameObject.GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        if (GameManager.industry >= 30)
+        {
+            thisImage.sprite = spaceStationSprite;
+        } else if (GameManager.industry >= 15)
+        {
+            thisImage.sprite = airPortSprite;
+        }
+        else
+        {
+            thisImage.sprite = trainStationSprite;
+        }
+    }
+
+}
