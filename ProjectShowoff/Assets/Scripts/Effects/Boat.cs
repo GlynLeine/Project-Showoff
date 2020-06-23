118648ae9ae7469fdf85e9fa7e354d34bc79ecc0
Robert O'Connor <445921@student.saxion.nl>
2020-06-23 19:42:54 +0200

    [ADD] Boat Spline and Logic

---
+++ ProjectShowoff/Assets/Scripts/Effects/Boat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    public Spline boatSpline;
+
+    float distance;
+    public float speed;
+    float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distance > boatSpline.length)
+        {
+            direction = -1f;
+        }
+        else if(distance < 0)
+        {
+            direction = 1f;
+        }
+        distance += speed * direction * Time.deltaTime;
+        transform.position = boatSpline.GetWorldPointAtDistance(distance);
+    }
+}
