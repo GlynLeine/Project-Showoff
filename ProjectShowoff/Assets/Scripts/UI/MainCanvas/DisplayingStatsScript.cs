5cd8e73b48b17be747a85fedabf26f8b4f27d0ed
Rick Oosthof <roosthof@gmail.com>
2020-06-23 22:18:52 +0200

    fixed stat display i hope, fixed train station to airport icons

---
+++ ProjectShowoff/Assets/Scripts/UI/MainCanvas/DisplayingStatsScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayingStatsScript : MonoBehaviour
+{
+    public RectTransform happinessFill;
+    public RectTransform pollutionFill;
+    public GameObject industryImage;
+    
+
+    public float happiness;
+    public float pollution;
+
+    private void OnValidate()
+    {
+        SetHappiness(happiness);
+        SetPollution(pollution);
+    }
+
+    public void SetIndustry(bool value){
+        if (value)
+        {
+            industryImage.SetActive(true);
+        }
+        else
+        {
+            industryImage.SetActive(false);
+        }
+    }
+    public void SetHappiness(float value)
+    {
+        happiness = Mathf.Clamp(value, -1f, 1f);
+        happinessFill.offsetMin = new Vector2(GameManager.lerp(160, 0, GameManager.smoothstep(0, -1, happiness)), 0);
+        happinessFill.offsetMax = new Vector2(GameManager.lerp(-160, 0, GameManager.smoothstep(0, 1, happiness)), 0);
+    }
+
+    public void SetPollution(float value)
+    {
+        pollution = Mathf.Clamp(value, -1f, 1f);
+        pollutionFill.offsetMin = new Vector2(GameManager.lerp(160, 0, GameManager.smoothstep(0, -1, pollution)), 0);
+        pollutionFill.offsetMax = new Vector2(GameManager.lerp(-160, 0, GameManager.smoothstep(0, 1, pollution)), 0);
+    }
+}
