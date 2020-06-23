af9507600601f185eacd91706d37dadc05ee9c55
Rick Oosthof <roosthof@gmail.com>
2020-06-23 19:23:19 +0200

    ! yea baby !! (made voiceline controller not play post end screen)

---
+++ ProjectShowoff/Assets/Scripts/UI/EndScreen/VoiceLineSetterEndScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineSetterEndScreen : MonoBehaviour
+{
+    private AnnouncerText announcerText;
+    private bool english;
+    void Start()
+    {
+        announcerText = gameObject.GetComponent<AnnouncerText>();
+    }
+
+    private void OnEnable()
+    {
+        if (LanguageSelector.LanguageSelected == LanguageSelector.LanguageSelectorSelected.English)
+        {
+            english = true;
+        }
+        StartCoroutine(WaitASecond());
+    }
+
+    IEnumerator WaitASecond()
+    {
+        while (gameObject.activeSelf)
+        {
+            if (english)
+            {
+                announcerText.TextChanger("Thank you for playing!");
+            }
+            else
+            {
+                announcerText.TextChanger("Dankjewel voor het spelen!");
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
